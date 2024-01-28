4d7e93b5c22a59cb1e172cd5c524353a640622a7
SenHou <shou@vnetsolutions.com.au>
2024-01-28 13:26:45 +1100

    init project

---
+++ src/main/java/sen/pokerhand/exercise/PokerHandSorter.java
@@ -0,0 +1,7 @@
+package sen.pokerhand.exercise;
+
+public class PokerHandSorter {
+    public static void main(String[] args) {
+        System.out.println("This is a poker hand sorter game.");
+    }
+}
