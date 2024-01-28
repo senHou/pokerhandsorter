3b72dd7d004e222861e9faf18551bd5356ea0d37
SenHou <shou@vnetsolutions.com.au>
2024-01-28 13:36:58 +1100

    add constant class

---
+++ src/main/java/sen/pokerhand/exercise/constant/PokerHandTypeConstant.java
@@ -0,0 +1,19 @@
+package sen.pokerhand.exercise.constant;
+
+/**
+ * A constant class that contains all the constant value
+ */
+public class PokerHandTypeConstant {
+    public final static int ROYAL_FLUSH = 10;
+    public final static int STRAIGHT_FLUSH = 9;
+    public final static int FOUR_OF_A_KIND = 8;
+    public final static int FULL_HOUSE = 7;
+    public final static int FLUSH = 6;
+    public final static int STRAIGHT = 5;
+    public final static int THREE_OF_A_KIND = 4;
+    public final static int TWO_PAIRS = 3;
+    public final static int PAIR = 2;
+    public final static int HIGH_CARD = 1;
+
+    public final static int T = 10;
+}
