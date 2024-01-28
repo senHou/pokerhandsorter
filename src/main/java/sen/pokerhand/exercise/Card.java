6624f7aaa5057d1c2fd37bbd84412554dcc18ac2
SenHou <shou@vnetsolutions.com.au>
2024-01-28 13:34:14 +1100

    add poker hand interface and a card class

---
+++ src/main/java/sen/pokerhand/exercise/Card.java
@@ -0,0 +1,51 @@
+package sen.pokerhand.exercise;
+
+import java.util.Objects;
+
+/**
+ * Card class
+ * A card contains a value and a suite
+ */
+public class Card implements Comparable<Card>{
+    private int value;
+    private String suite;
+
+    public Card(int value, String suite) {
+        this.value = value;
+        this.suite = suite;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    @Override
+    public int compareTo(Card card) {
+        return this.value - card.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value && Objects.equals(suite, card.suite);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, suite);
+    }
+}
---
+++ src/main/java/sen/pokerhand/exercise/PokerHand.java
@@ -0,0 +1,36 @@
+package sen.pokerhand.exercise;
+
+import java.util.List;
+
+/**
+ * PokerHand Interface.
+ * Declare required methods for poker hand
+ */
+public interface PokerHand {
+
+    int getRank();
+
+    int getPair();
+
+    void setPair(int pair);
+
+    int getHighPair();
+
+    void setHighPair(int highPair);
+
+    int getLowPair();
+
+    void setLowPair(int lowPair);
+
+    int getThreeOfKind();
+
+    void setThreeOfKind(int threeOfKind);
+
+    int getFourOfKind();
+
+    void setFourOfKind(int fourOfKind);
+
+    List<Card> getSingleCardList();
+
+    List<Card> getCardList();
+}
